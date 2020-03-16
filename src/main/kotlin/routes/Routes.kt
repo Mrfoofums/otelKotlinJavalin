eef005c8bb6210c51f1de0ce2a5d75db7781c576
Forrest <forrest@longviewacquisitions.com>
2020-03-16 12:11:17 -0400

    stuff

--- src/main/kotlin/routes/Routes.kt
+++ src/main/kotlin/routes/Routes.kt
@@ -1,39 +1,87 @@
 package routes
 
+import io.grpc.ManagedChannelBuilder
 import io.javalin.Javalin
 import io.javalin.apibuilder.ApiBuilder.get
 import io.javalin.http.Context
+import io.opentelemetry.exporters.jaeger.JaegerGrpcSpanExporter
+import io.opentelemetry.exporters.logging.LoggingExporter
 import io.opentelemetry.sdk.OpenTelemetrySdk
+import io.opentelemetry.sdk.trace.SpanProcessor
+import io.opentelemetry.sdk.trace.export.SimpleSpansProcessor
+import io.opentelemetry.trace.Span
 import io.opentelemetry.trace.Tracer
+import kotlin.collections.HashMap
+
 
 data class Move (val description: String ="", val type: String = "")
 
 val tracer = getTracer("Kotlin Test")
+val spanMap =  mutableMapOf<String, Span>()
 
 fun getTracer(service: String): Tracer {
-    return OpenTelemetrySdk.getTracerFactory().get("KotlinTracer")
+    val tracer = OpenTelemetrySdk.getTracerFactory().get("KotlinTracer")
+    val jaegerProcessor: SpanProcessor = SimpleSpansProcessor.newBuilder(
+        JaegerGrpcSpanExporter.newBuilder()
+            .setServiceName(service)
+            .setChannel(
+                ManagedChannelBuilder.forAddress(
+                    "localhost", 14250
+                ).usePlaintext().build()
+            )
+            .build()
+    ).build()
+
+    val logProcessor: SpanProcessor = SimpleSpansProcessor.newBuilder(LoggingExporter()).build()
+
+    OpenTelemetrySdk.getTracerFactory().addSpanProcessor(logProcessor)
+    OpenTelemetrySdk.getTracerFactory().addSpanProcessor(jaegerProcessor)
+
+    return tracer
 }
+
 fun createRoutes(){
 
-    val handler = MoveRequestHandler(tracer)
+
+    val handler = MoveRequestHandler()
 
     val app = Javalin.create { config ->
         config.defaultContentType = "application/json"
         config.dynamicGzip = true
         config.contextPath = "/api/v1"
     }.routes{
-       get("/moves/:move"){ctx: Context -> ctx.json(handler.getMoveByName(ctx))}
-        get("/moves/"){ctx: Context -> ctx.json(handler.getAllMoves())}
+       get("/moves/:move"){ctx: Context ->
+           val span = tracer.spanBuilder("beforeSpan").startSpan()
+           tracer.withSpan(span)
+           try {
+               ctx.json(handler.getMoveByName(ctx,span))
+           }
+           finally {
+               span.end()
+           }
+       }
+        get("/moves/"){ctx: Context ->
+
+            val span = tracer.spanBuilder("beforeSpan").startSpan()
+            try {
+                ctx.json(handler.getAllMoves(span))
+            }
+            finally {
+                span.end()
+            }
+        }
     }
 
     app.before { ctx ->
-        //Extract Context here
+//        tracer.spanBuilder(ctx.fullUrl()).startSpan().also {
+//            it.setAttribute("context","before")
+//        }
+
     }
 
-    app.after{ ctx->
-//        tracer.activeSpan().finish()
-
-        //Close our span and inject context
+    app.after{
+//        spanMap["beforeSpan"]?.end()
+//        spanMap.clear()
     }
 
     app.error(404) { ctx->
@@ -41,24 +89,41 @@ fun createRoutes(){
     }.start(1991)
 }
 
-class MoveRequestHandler(tracer: Tracer) {
+class MoveRequestHandler{
 
-    private val moveDAO = MoveDAO(tracer)
+    private val moveDAO = MoveDAO()
 
-    fun getMoveByName(ctx: Context):Move {
-
+    fun getMoveByName(ctx: Context, parentSpant: Span):Move {
         val moveName = ctx.pathParam("move")
-        return moveDAO.getMoveByName(moveName)
+
+        tracer.spanBuilder("getMoveByNameSpanHandler").setParent(parentSpant).startSpan().also {
+            it.setAttribute("move", moveName)
+            try {
+                return moveDAO.getMoveByName(moveName, it)
+            }
+            finally {
+                it.end()
+            }
+        }
+
     }
 
-    fun getAllMoves(): HashMap<String, Move> {
+    fun getAllMoves(parentSpan: Span): HashMap<String, Move> {
+        tracer.spanBuilder("getAllMovesSpan").setParent(parentSpan).startSpan().also {
+            it.setAttribute("move", "allMoves")
+            try {
+                return  moveDAO.moves
+            }
+            finally {
+                it.end()
+            }
+        }
 
-        return moveDAO.moves
     }
 
 }
 
-class MoveDAO (tracer: Tracer)  {
+class MoveDAO   {
 
      val moves = hashMapOf(
         "windmill" to Move(
@@ -69,8 +134,16 @@ class MoveDAO (tracer: Tracer)  {
         "toprock" to Move("The Top Rock is all movement where the breaker is still standing. Set's are typically started with top rock.", "Rocking")
     )
 
-    fun getMoveByName(moveName: String): Move {
-
-        return moves.getValue(moveName)
+    fun getMoveByName(moveName: String, parentSpan: Span): Move {
+        println("ParnetSpan ID passed into GetMoveByNameSpan id: ${tracer.currentSpan.context.spanId}")
+        tracer.spanBuilder("getMoveByNameDAOSpan").setParent(parentSpan).startSpan().also {
+            it.setAttribute("move", moveName)
+            try{
+                return moves.getValue(moveName)
+            }
+            finally {
+                it.end()
+            }
+        }
     }
 }
